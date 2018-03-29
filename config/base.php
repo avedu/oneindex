a78f2dc0b119a0f25956767ec2fb0549cfa50052
donwa <hi0660@gmail.com>
2018-03-29 19:55:49 +0800

    Update base.php
    增加cache类型设置

--- config/base.php
+++ config/base.php
@@ -2,7 +2,8 @@
   'client_id' => 'ea2b36f6-b8ad-40be-bc0f-e5e4a4a7d4fa',
   'client_secret' => 'EIVCx5ztMSxMsga18MQ7rmGf9EIP7zv6tfimb0Kp5Uc=',
   'redirect_uri' => 'https://ju.tn/onedrive-login',
-  'cache_expire_time' => 300,
-  'cache_refresh_time' => 30,
+  'cache_type'=> 'file',//sqlite 需要pdo 支持
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
   'root_path' => '?'
 );
