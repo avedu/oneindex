a3b57ea20d8f50133424907b30fd98eff8aebb4e
Max Sum <maxsumprc@gmail.com>
2018-03-29 21:32:54 +0800

    Remove space

--- view/list.php
+++ view/list.php
@@ -35,8 +35,8 @@ $root_path = get_absolute_path(dirname($_SERVER['SCRIPT_NAME'])).config('root_pa
 					<tr>
 						<td class="file-name"><a class="icon icon-file" href="<?php echo get_absolute_path($root_path.$path).$item['name'];?>"><?php echo $item['name'];?></a></td>
 						<td class="file-size"><?php echo $item['size'];?></td>
-						<td class="file-date-created">&nbsp;<?php echo date("Y-m-d H:i:s", $item['createdDateTime']);?></td>
-						<td class="file-date-modified">&nbsp;<?php echo date("Y-m-d H:i:s", $item['lastModifiedDateTime']);?></td>
+						<td class="file-date-created"><?php echo date("Y-m-d H:i:s", $item['createdDateTime']);?></td>
+						<td class="file-date-modified"><?php echo date("Y-m-d H:i:s", $item['lastModifiedDateTime']);?></td>
 					</tr>
 				<?php endif;?>
 			<?php endforeach;?>
