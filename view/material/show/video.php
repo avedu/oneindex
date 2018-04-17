f92f2111c0f3627ad9d2f033b993c86efb590fdd
donwa <hi0660@gmail.com>
2018-04-17 20:04:09 +0800

    视频显示修复

--- view/material/show/video.php
+++ view/material/show/video.php
@@ -5,13 +5,13 @@
 <div class="mdui-container-fluid">
 	<br>
 	<video class="mdui-video-fluid mdui-center" controls autoplay>
-	  <source src="<?php e($url);?>" type="video/mp4">
+	  <source src="<?php e($item['downloadUrl']);?>" type="video/mp4">
 	</video>
 	<br>
 	<!-- 固定标签 -->
 	<div class="mdui-textfield">
 	  <label class="mdui-textfield-label">引用地址</label>
-	  <textarea class="mdui-textfield-input"><video><source src="<?php e($item['downloadUrl']);?>" type="video/mp4"></video></textarea>
+	  <textarea class="mdui-textfield-input"><video><source src="<?php e($url);?>" type="video/mp4"></video></textarea>
 	</div>
 </div>
 <a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
