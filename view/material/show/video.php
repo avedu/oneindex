81e52e52b2392e1876d60f7590b7b23736518d05
donwa <hi0660@gmail.com>
2018-04-18 18:26:43 +0800

    小预览图，不自动播放

--- view/material/show/video.php
+++ view/material/show/video.php
@@ -3,7 +3,7 @@
 <?php view::begin('content');?>
 <div class="mdui-container-fluid">
 	<br>
-	<video class="mdui-video-fluid mdui-center" preload controls autoplay poster="<?php @e($item['thumbnails'][0]['large']['url']);?>">
+	<video class="mdui-video-fluid mdui-center" preload controls poster="<?php @e($item['thumbnails'][0]['medium']['url']);?>">
 	  <source src="<?php e($item['downloadUrl']);?>" type="video/mp4">
 	</video>
 	<br>
