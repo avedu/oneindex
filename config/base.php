02fd7d19816e6f6ca83d6cacbd4773290270c7ce
donwa <hi0660@gmail.com>
2018-04-19 22:10:18 +0800

    delete site_name

--- config/base.php
+++ config/base.php
@@ -1,5 +1,4 @@
 <?php return array (
-  'site_name' => 'one',
   'client_id' => 'ea2b36f6-b8ad-40be-bc0f-e5e4a4a7d4fa',
   'client_secret' => 'EIVCx5ztMSxMsga18MQ7rmGf9EIP7zv6tfimb0Kp5Uc=',
   'redirect_uri' => 'https://ju.tn/onedrive-login',
