22c1912fe613944fe862992d0bad058cb5de5c1f
donwa <jluzh@qq.com>
2018-05-22 10:53:22 +0800

    安装失败提示

--- view/material/admin/install_3.php
+++ view/material/admin/install_3.php
@@ -13,7 +13,7 @@
 	<a class="mdui-btn mdui-color-theme-accent mdui-ripple mdui-float-left" href="./"><i class="mdui-icon material-icons">&#xe2c7;</i> 访问网站</a>
 	<a class="mdui-btn mdui-color-theme-accent mdui-ripple mdui-float-right" href="?/admin/" ><i class="mdui-icon material-icons">&#xe8b8;</i> 管理后台</a>
 <?php else:?>
-	<h1>程序安装成功!</h1>
+	<h1>程序安装失败!</h1>
 	<br><br>
 	<a class="mdui-btn mdui-btn-raised mdui-float-left" href="?step=2">重新绑定</a>
 <?php endif;?>
