2b0a7b4a74189a25ae1d47f1e3b3dcfc4728a7b0
donwa <jluzh@qq.com>
2018-06-06 15:12:29 +0800

    图床二级目录问题。

--- controller/ImagesController.php
+++ controller/ImagesController.php
@@ -4,16 +4,16 @@ class ImagesController{
 	function index(){
 		if($this->is_image($_FILES["file"]) ){
 			$content = file_get_contents( $_FILES["file"]['tmp_name']);
-			$remotepath =  '/images/'.date('Y/m/d/');
+			$remotepath =  'images/'.date('Y/m/d/');
 			$remotefile = $remotepath.$_FILES["file"]['name'];
-			$result = onedrive::upload($remotefile, $content);
+			$result = onedrive::upload(config('onedrive_root').$remotefile, $content);
 			
 			if($result){
-				$cachefile = CACHE_PATH . md5('dir_'.$remotepath) . '.php';
+				$cachefile = CACHE_PATH . md5('dir_'.config('onedrive_root').$remotepath) . '.php';
 				unlink($cachefile);
 				$root = get_absolute_path(dirname($_SERVER['SCRIPT_NAME'])).config('root_path');
 				$http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
-				$url = $http_type.$_SERVER['HTTP_HOST'].$root.$remotefile.((config('root_path') == '?')?'&s':'?s');
+				$url = $http_type.$_SERVER['HTTP_HOST'].$root.'/'.$remotefile.((config('root_path') == '?')?'&s':'?s');
 				view::direct($url);
 			}
 		}
