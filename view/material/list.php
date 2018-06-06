e235bfb8e5f32266b76482906322a8e99ccc136f
akkuman <1075768094@qq.com>
2018-06-07 01:03:10 +0800

    fix issue/124

--- view/material/list.php
+++ view/material/list.php
@@ -61,7 +61,7 @@ function file_ico($item){
 		</li>
 			<?php else:?>
 		<li class="mdui-list-item file mdui-ripple">
-			<a href="<?php echo get_absolute_path($root.$path).urlencode($item['name']);?>" target="_blank">
+			<a href="<?php echo get_absolute_path($root.$path).rawurlencode($item['name']);?>" target="_blank">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons"><?php echo file_ico($item);?></i>
 		    	<?php e($item['name']);?>
