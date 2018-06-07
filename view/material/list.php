172433475d4b2ac0e56b56fb1a9edb06c3994528
donwa <jluzh@qq.com>
2018-06-07 10:33:21 +0800

    文件夹特殊字符fix

--- view/material/list.php
+++ view/material/list.php
@@ -50,7 +50,7 @@ function file_ico($item){
 			<?php if(!empty($item['folder'])):?>
 
 		<li class="mdui-list-item mdui-ripple">
-			<a href="<?php echo get_absolute_path($root.$path.$item['name']);?>">
+			<a href="<?php echo get_absolute_path($root.$path.rawurlencode($item['name']));?>">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons">folder_open</i>
 		    	<?php e($item['name']);?>
