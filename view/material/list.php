ef3e1014a39f94bc435a051b72e74046356a2dce
Rhilip <rhilipruan@gmail.com>
2018-07-17 08:58:39 +0800

    添加前端排序

--- view/material/list.php
+++ view/material/list.php
@@ -29,9 +29,9 @@ function file_ico($item){
 <div class="mdui-row">
 	<ul class="mdui-list">
 		<li class="mdui-list-item th">
-		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件</div>
-		  <div class="mdui-col-sm-3 mdui-text-right">修改时间</div>
-		  <div class="mdui-col-sm-2 mdui-text-right">大小</div>
+		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件 <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="downward">arrow_downward</i></div>
+		  <div class="mdui-col-sm-3 mdui-text-right">修改时间 <i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">arrow_downward</i></div>
+		  <div class="mdui-col-sm-2 mdui-text-right">大小 <i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">arrow_downward</i></div>
 		</li>
 		<?php if($path != '/'):?>
 		<li class="mdui-list-item mdui-ripple">
@@ -45,11 +45,11 @@ function file_ico($item){
 		  	</a>
 		</li>
 		<?php endif;?>
-		
+
 		<?php foreach((array)$items as $item):?>
 			<?php if(!empty($item['folder'])):?>
 
-		<li class="mdui-list-item mdui-ripple">
+		<li class="mdui-list-item mdui-ripple" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
 			<a href="<?php echo get_absolute_path($root.$path.rawurlencode($item['name']));?>">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons">folder_open</i>
@@ -60,7 +60,7 @@ function file_ico($item){
 		  	</a>
 		</li>
 			<?php else:?>
-		<li class="mdui-list-item file mdui-ripple">
+		<li class="mdui-list-item file mdui-ripple" data-sort data-sort-name="<?php e($item['name']);?>" data-sort-date="<?php echo $item['lastModifiedDateTime'];?>" data-sort-size="<?php echo $item['size'];?>">
 			<a href="<?php echo get_absolute_path($root.$path).rawurlencode($item['name']);?>" target="_blank">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons"><?php echo file_ico($item);?></i>
@@ -86,16 +86,55 @@ function file_ico($item){
 </div>
 <script>
 $ = mdui.JQ;
-$(function(){
-	$('.file a').each(function(){
-		$(this).on('click', function () {
-			var form = $('<form target=_blank method=post></form>').attr('action', $(this).attr('href')).get(0);
-			$(document.body).append(form);
-			form.submit();
-			$(form).remove();
-			return false;
-		});
-	});
+
+$.fn.extend({
+    sortElements: function (comparator, getSortable) {
+        getSortable = getSortable || function () { return this; };
+
+        var placements = this.map(function () {
+            var sortElement = getSortable.call(this),
+                parentNode = sortElement.parentNode,
+                nextSibling = parentNode.insertBefore(
+                    document.createTextNode(''),
+                    sortElement.nextSibling
+                );
+
+            return function () {
+                parentNode.insertBefore(this, nextSibling);
+                parentNode.removeChild(nextSibling);
+            };
+        });
+
+        return [].sort.call(this, comparator).each(function (i) {
+            placements[i].call(getSortable.call(this));
+        });
+    }
+});
+
+$(function () {
+    $('.file a').each(function () {
+        $(this).on('click', function () {
+            var form = $('<form target=_blank method=post></form>').attr('action', $(this).attr('href')).get(0);
+            $(document.body).append(form);
+            form.submit();
+            $(form).remove();
+            return false;
+        });
+    });
+
+    $('.icon-sort').on('click', function () {
+        let sort_type = $(this).attr("data-sort"), sort_order = $(this).attr("data-order");
+        let sort_order_to = (sort_order === "downward") ? "upward" : "downward";
+
+        $('li[data-sort]').sortElements(function (a, b) {
+            let data_a = $(a).attr("data-sort-" + sort_type), data_b = $(b).attr("data-sort-" + sort_type);
+            let rt = data_a.localeCompare(data_b, undefined, {numeric: true});
+            return (sort_order === "downward") ? 0-rt : rt;
+        });
+
+        $(this).attr("data-order", sort_order_to).text("arrow_" + sort_order_to);
+    })
+
 });
 </script>
 <?php view::end('content');?>
