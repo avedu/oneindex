d09d56dbb1fb8a7694f2c949d7701dbddcb2fc9c
donwa <hi0660@gmail.com>
2018-09-04 23:09:56 +0800

    nexmoe 增加缩略图浏览功能(测试)

--- view/material/list.php
+++ view/material/list.php
@@ -29,9 +29,9 @@ function file_ico($item){
 <div class="mdui-row">
 	<ul class="mdui-list">
 		<li class="mdui-list-item th">
-		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件 <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="downward">arrow_downward</i></div>
-		  <div class="mdui-col-sm-3 mdui-text-right">修改时间 <i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">arrow_downward</i></div>
-		  <div class="mdui-col-sm-2 mdui-text-right">大小 <i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">arrow_downward</i></div>
+		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件 <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="downward">expand_more</i></div>
+		  <div class="mdui-col-sm-3 mdui-text-right">修改时间 <i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i></div>
+		  <div class="mdui-col-sm-2 mdui-text-right">大小 <i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">expand_more</i></div>
 		</li>
 		<?php if($path != '/'):?>
 		<li class="mdui-list-item mdui-ripple">
@@ -87,30 +87,6 @@ function file_ico($item){
 <script>
 $ = mdui.JQ;
 
-$.fn.extend({
-    sortElements: function (comparator, getSortable) {
-        getSortable = getSortable || function () { return this; };
-
-        var placements = this.map(function () {
-            var sortElement = getSortable.call(this),
-                parentNode = sortElement.parentNode,
-                nextSibling = parentNode.insertBefore(
-                    document.createTextNode(''),
-                    sortElement.nextSibling
-                );
-
-            return function () {
-                parentNode.insertBefore(this, nextSibling);
-                parentNode.removeChild(nextSibling);
-            };
-        });
-
-        return [].sort.call(this, comparator).each(function (i) {
-            placements[i].call(getSortable.call(this));
-        });
-    }
-});
-	
 $(function () {
     $('.file a').each(function () {
         $(this).on('click', function () {
@@ -123,16 +99,16 @@ $(function () {
     });
 
     $('.icon-sort').on('click', function () {
-        let sort_type = $(this).attr("data-sort"), sort_order = $(this).attr("data-order");
-        let sort_order_to = (sort_order === "downward") ? "upward" : "downward";
+        var sort_type = $(this).attr("data-sort"), sort_order = $(this).attr("data-order");
+        var sort_order_to = (sort_order === "less") ? "more" : "less";
 
         $('li[data-sort]').sortElements(function (a, b) {
-            let data_a = $(a).attr("data-sort-" + sort_type), data_b = $(b).attr("data-sort-" + sort_type);
-            let rt = data_a.localeCompare(data_b, undefined, {numeric: true});
-            return (sort_order === "downward") ? 0-rt : rt;
+            var data_a = $(a).attr("data-sort-" + sort_type), data_b = $(b).attr("data-sort-" + sort_type);
+            var rt = data_a.localeCompare(data_b, undefined, {numeric: true});
+            return (sort_order === "more") ? 0-rt : rt;
         });
 
-        $(this).attr("data-order", sort_order_to).text("arrow_" + sort_order_to);
+        $(this).attr("data-order", sort_order_to).text("expand_" + sort_order_to);
     })
 
 });
--- view/nexmoe/list.php
+++ view/nexmoe/list.php
@@ -23,14 +23,47 @@ function file_ico($item){
 	<?php e($head);?>
 </div>
 <?php endif;?>
+<style>
+.thumb .th{
+	display: none;
+}
+.thumb .mdui-text-right{
+	display: none;
+}
+.thumb .mdui-list-item a ,.thumb .mdui-list-item {
+	width:217px;
+	height: 230px;
+	float: left;
+	margin: 10px 10px !important;
+}
 
+.thumb .mdui-col-xs-12,.thumb .mdui-col-sm-7{
+	width:100% !important;
+	height:230px;
+}
+
+.thumb .mdui-list-item .mdui-icon{
+	font-size:100px;
+	display: block;
+	margin-top: 40px;
+	color: #7ab5ef;
+}
+.thumb .mdui-list-item span{
+	float: left;
+	display: block;
+	text-align: center;
+	width:100%;
+	position: absolute;
+    top: 180px;
+}
+</style>
 <div class="nexmoe-item">
 <div class="mdui-row">
 	<ul class="mdui-list">
 		<li class="mdui-list-item th">
-		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件</div>
-		  <div class="mdui-col-sm-3 mdui-text-right">修改时间</div>
-		  <div class="mdui-col-sm-2 mdui-text-right">大小</div>
+		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件 <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="downward">expand_more</i></div>
+		  <div class="mdui-col-sm-3 mdui-text-right">修改时间 <i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i></div>
+		  <div class="mdui-col-sm-2 mdui-text-right">大小 <i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">expand_more</i></div>
 		</li>
 		<?php if($path != '/'):?>
 		<li class="mdui-list-item mdui-ripple">
@@ -52,7 +85,7 @@ function file_ico($item){
 			<a href="<?php echo get_absolute_path($root.$path.rawurlencode($item['name']));?>">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons">folder_open</i>
-		    	<?php e($item['name']);?>
+		    	<span><?php e($item['name']);?></span>
 			  </div>
 			  <div class="mdui-col-sm-3 mdui-text-right"><?php echo date("Y-m-d H:i:s", $item['lastModifiedDateTime']);?></div>
 			  <div class="mdui-col-sm-2 mdui-text-right"><?php echo onedrive::human_filesize($item['size']);?></div>
@@ -63,7 +96,7 @@ function file_ico($item){
 			<a href="<?php echo get_absolute_path($root.$path).rawurlencode($item['name']);?>" target="_blank">
 			  <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 				<i class="mdui-icon material-icons"><?php echo file_ico($item);?></i>
-		    	<?php e($item['name']);?>
+		    	<span><?php e($item['name']);?></span>
 			  </div>
 			  <div class="mdui-col-sm-3 mdui-text-right"><?php echo date("Y-m-d H:i:s", $item['lastModifiedDateTime']);?></div>
 			  <div class="mdui-col-sm-2 mdui-text-right"><?php echo onedrive::human_filesize($item['size']);?></div>
@@ -87,6 +120,30 @@ function file_ico($item){
 <script>
 $ = mdui.JQ;
 
+$.fn.extend({
+    sortElements: function (comparator, getSortable) {
+        getSortable = getSortable || function () { return this; };
+
+        var placements = this.map(function () {
+            var sortElement = getSortable.call(this),
+                parentNode = sortElement.parentNode,
+                nextSibling = parentNode.insertBefore(
+                    document.createTextNode(''),
+                    sortElement.nextSibling
+                );
+
+            return function () {
+                parentNode.insertBefore(this, nextSibling);
+                parentNode.removeChild(nextSibling);
+            };
+        });
+
+        return [].sort.call(this, comparator).each(function (i) {
+            placements[i].call(getSortable.call(this));
+        });
+    }
+});
+
 function downall() {
      let dl_link_list = Array.from(document.querySelectorAll("li a"))
          .map(x => x.href) // 所有list中的链接
@@ -100,8 +157,28 @@ function downall() {
      a.download = "folder_download_link.txt"; // 文件名称，你可以根据你的需要构造
      a.click() // 模拟点击
      a.remove();
+}
 
- }	
+function thumb(){
+	if($('.mdui-fab i').text() == "apps"){
+		$('.mdui-fab i').text("format_list_bulleted");
+		$('.nexmoe-item').removeClass('thumb');
+		$('.nexmoe-item .mdui-icon').show();
+		$('.nexmoe-item .mdui-list-item').css("background","");
+	}else{
+		$('.mdui-fab i').text("apps");
+		$('.nexmoe-item').addClass('thumb');
+		$('.mdui-col-xs-12 i.mdui-icon').each(function(){
+			if($(this).text() == "image" || $(this).text() == "ondemand_video"){
+				var href = $(this).parent().parent().attr('href');
+				var thumb =(href.indexOf('?') == -1)?'?t=220':'&t=220';
+				$(this).hide();
+				$(this).parent().parent().parent().css("background","url("+href+thumb+")  no-repeat center top");
+			}
+		});
+	}
+
+}	
 
 $(function(){
 	$('.file a').each(function(){
@@ -113,7 +190,21 @@ $(function(){
 			return false;
 		});
 	});
+
+	$('.icon-sort').on('click', function () {
+        let sort_type = $(this).attr("data-sort"), sort_order = $(this).attr("data-order");
+        let sort_order_to = (sort_order === "less") ? "more" : "less";
+
+        $('li[data-sort]').sortElements(function (a, b) {
+            let data_a = $(a).attr("data-sort-" + sort_type), data_b = $(b).attr("data-sort-" + sort_type);
+            let rt = data_a.localeCompare(data_b, undefined, {numeric: true});
+            return (sort_order === "more") ? 0-rt : rt;
+        });
+
+        $(this).attr("data-order", sort_order_to).text("expand_" + sort_order_to);
+    });
+
 });
 </script>
-<a href="javascript:downall();" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+<a href="javascript:thumb();" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">format_list_bulleted</i></a>
 <?php view::end('content');?>
