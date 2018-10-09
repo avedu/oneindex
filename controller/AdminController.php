2ac7aaa4a62d1f55fd09425029f50c8c6c39207d
Secret <i@wangmao.me>
2018-10-09 11:33:53 +0800

    fix login page title

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -30,7 +30,7 @@ class AdminController{
 			setcookie('admin', md5(config('password').config('refresh_token')) );
 			return view::direct(get_absolute_path(dirname($_SERVER['SCRIPT_NAME'])).'?/admin/');
 		}
-		return view::load('login');
+		return view::load('login')->with('title', '系统管理');
 	}
 
 	function logout(){
