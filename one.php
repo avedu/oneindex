10c58a7bf2c1aabbf63b94a01d0ec99c2c2edd3a
donwa <hi0660@gmail.com>
2018-10-19 22:43:18 +0800

    Update one.php
    fix cache clear

--- one.php
+++ one.php
@@ -7,10 +7,7 @@ ini_set('memory_limit', '128M');
 
 class one{
 	static function cache_clear(){
-		$dir=opendir(CACHE_PATH);
-		while ($file=readdir($dir)) {
-			@unlink(CACHE_PATH.$file);
-		}
+		cache::clear();
 	}
 
 	static function cache_refresh(){
